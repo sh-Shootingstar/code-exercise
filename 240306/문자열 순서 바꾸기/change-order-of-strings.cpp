326f78142f90843fac6e7f4e1c537b16937b7c2d
한승한 <153101673+sh-Shootingstar@users.noreply.github.com>
2024-03-06 19:08:59 +0900

    Title: 문자열 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string s,t;
+    cin >> s >> t;
+    cout << t << endl;
+    cout << s;
+
+
+
+
+
+    return 0;
+}
