3552fa7d08b6ba0fbc7e1dbaaad6e616728ec030
한승한 <153101673+sh-Shootingstar@users.noreply.github.com>
2024-03-06 18:59:09 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double a,b,c;
+    cin >> a >> b >> c;
+
+    cout <<fixed;
+    cout.precision(3);
+    cout << a << endl << b << endl << c;
+    
+
+
+     return 0;
+}
