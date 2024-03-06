421e2974c37b77d2cac933643ee5c6df03608c47
한승한 <153101673+sh-Shootingstar@users.noreply.github.com>
2024-03-06 18:42:52 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+   
+   cout << fixed;
+
+   double n;
+   cin >> n;
+
+   cout.precision(2);
+   cout << n;
+
+
+
+    return 0;
+}
