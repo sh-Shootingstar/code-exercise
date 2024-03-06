44565733cd33c1d5013638e6f39cdfae9dd31e06
한승한 <153101673+sh-Shootingstar@users.noreply.github.com>
2024-03-06 19:18:09 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+  
+    int a ,b;
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+
+    cout << a << b;
+
+
+
+
+    return 0;
+}
