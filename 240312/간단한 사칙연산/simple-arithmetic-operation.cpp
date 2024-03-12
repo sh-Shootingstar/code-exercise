7e62bfa74fa0a8563902077799e28d51d3f40c87
한승한 <153101673+sh-Shootingstar@users.noreply.github.com>
2024-03-12 19:28:04 +0900

    Title: 간단한 사칙연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240312/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a; 
+    int b;
+    
+    cin >> a >> b;
+    
+    cout << a + b << endl;
+    cout << a -b << endl;
+    cout << a/b << endl;
+    cout << a% b ;
+    
+    
+
+
+    return 0;
+}
