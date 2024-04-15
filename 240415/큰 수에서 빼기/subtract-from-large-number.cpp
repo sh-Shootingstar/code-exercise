add19d1313aec6e8256be9898584c36bd77378e3
한승한 <153101673+sh-Shootingstar@users.noreply.github.com>
2024-04-15 09:13:47 +0900

    Title: 큰 수에서 빼기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240415/큰 수에서 빼기/subtract-from-large-number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+int a, b;
+
+cin >> a >> b >> endl;
+
+if (a < b) {
+
+cout << b-a << endl;
+
+}
+
+else : cout << a-b << endl;
+
+
+
+
+
+
+
+
+    return 0;
+}
